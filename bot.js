dcfbd437f1567ec8605ff0015fd9fd2283d2a797
Sahad <89368799+sahadpp1@users.noreply.github.com>
2021-08-23 17:28:17 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 /* Copyright (C) 2020 Yusuf Usta.
-re edited by sahad & hisham & ameer
+re edited by sahad
 Licensed under the  GPL-3a.0 License;
 you may not use this file except in compliance with the License.
 
